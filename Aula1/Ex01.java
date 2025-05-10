5b604755218cd27cef2d4a1514a4b6792bfcd701
Luan <luantrsilva@gmail.com>
2025-05-10 10:48:51 -0300

    exercicios da aula 1

---
+++ Aula1/Ex01.java
@@ -0,0 +1,10 @@
+public class Ex01 
+{
+	public static void main(String[] args) {
+		String meuNome = "Luan";
+		String outroNome = "Ruan";
+		
+		boolean mesmoNome = meuNome == outroNome;
+		System.out.println("É o mesmo nome? " + mesmoNome);
+	}
+}
---
+++ Aula1/Ex02.java
@@ -0,0 +1,9 @@
+public class Ex02 {
+    public static void main (String[] args) {
+        int minhaIdade = 15;
+        int professorIdade = 20;
+        
+        boolean mesmaIdade = minhaIdade == professorIdade;
+        System.out.println("Tenho a mesma idade do professor? "+mesmaIdade);
+    }
+}
