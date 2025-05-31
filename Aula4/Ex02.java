d0e1e7915133a49d4a908e6006170f54c9b8269d
Luan <luantrsilva@gmail.com>
2025-05-31 10:20:23 -0300

    Exercício 2 - aula 4

---
+++ Aula4/Ex02.java
@@ -0,0 +1,7 @@
+public class Ex02 {
+    public static void main (String[] args) {
+        int numeros[] = {10, 20, 30};
+        int soma = numeros[0]+numeros[1]+numeros[2];
+        System.out.println("Soma: "+soma;
+    }
+}
